f8806f2b268b202b9b0749c2cb076df4f35fda7b
Mehdi Azabou <mehdiazabou@gmail.com>
2018-02-13 00:01:59 +0100

    diffrent RSS feeds from the NYT

--- crawl.py
+++ crawl.py
@@ -11,6 +11,7 @@ class scraper:
 # Initialize the crawler with the name of database
     def __init__(self,dbname):
         self.con = sqlite.connect(dbname)
+        self.rssList = {}
 
     def __del__(self):
         self.con.close()
@@ -18,29 +19,38 @@ class scraper:
     def dbcommit(self):
         self.con.commit()
 
-    def scrap(self,rssFeed):
-        rss = feedparser.parse(rssFeed)
-        for entry in rss.entries:
-            title = entry["title"]
-            author = entry["author"]
-            published = entry["published_parsed"]
-            url = entry["link"]
-            try:
-                c = urllib2.urlopen(url)
-            except:
-                print "Could not open %s" % url
-                continue
-            soup = BeautifulSoup(c.read(),"html.parser")
-            self.addtoindex(url, soup, title, author, published)
-            self.dbcommit()
+    def setFeedList(self,filename):
+       file = open(filename)
+       for line in file: #? for line in file
+            rssName, rssFeed = line.strip().split(' ')
+            self.rssList[rssName] = rssFeed
+       file.close()
 
-            # Index an individual page
-    def addtoindex(self, url, soup, title, author, published):
+    def scrap(self):
+        for rssName in self.rssList.keys():
+            rssFeed = self.rssList[rssName]
+            rss = feedparser.parse(rssFeed)
+            for entry in rss.entries:
+                title = entry["title"]
+                author = entry["author"]
+                published = entry["published_parsed"]
+                url = entry["link"]
+                try:
+                    c = urllib2.urlopen(url)
+                except:
+                    print "Could not open %s" % url
+                    continue
+                soup = BeautifulSoup(c.read(),"html.parser")
+                self.addtoindex(url, soup, title, rssName, author, published)
+                self.dbcommit()
+
+    # Index an individual page
+    def addtoindex(self, url, soup, title, source, author, published):
             if self.isindexed(url): return
             print 'Indexing ' + url
             # Get the individual words
             text = self.getArticle(soup)
-            text = text.encode('utf-8')
+            text = text.encode('utf-8') #probleme d'encodage rencontre
 
             filename = './src/'+title + '.txt'
             file = open(filename,'w')
@@ -50,7 +60,7 @@ class scraper:
             words = self.separatewords(text)
 
             # Get the URL id
-            cur = self.con.execute("insert into articleList (title,source,author,link,published,file) values ('%s', '%s', '%s', '%s', '%s', '%s')" % (title, 'NYT', author, published, url, filename))
+            cur = self.con.execute("insert into articleList (title,source,author,link,published,file) values ('%s', '%s', '%s', '%s', '%s', '%s')" % (title, source, author, url, published, filename))
             articleid = cur.lastrowid
 
             # Link each word to this url
@@ -94,13 +104,16 @@ class scraper:
 # Separate the words by any non-whitespace character
     def separatewords(self,text):
         splitter = re.compile('\\W*')
-        return [s.lower() for s in splitter.split(text) if s != '']
+        return self.filter([s.lower() for s in splitter.split(text) if s != ''])
+
+    def filter(self,words):
+        return [word for word in words if (len(word)>1 and word.isalpha())] #list comprehension
 
 # Return true if this url is already indexed
     def isindexed(self,url):
         u = self.con.execute("select rowid from articleList where link='%s'" % url).fetchone()
         return (u != None)
-            
+
 # Create the database tables
     def createindextables(self):
         self.con.execute('create table articleList(title,source,author,link,published,file)')
