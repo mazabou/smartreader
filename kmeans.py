36eb5ec906bc0ade7db8a08ece16f71614e7f7e6
Mehdi Azabou <mehdiazabou@gmail.com>
2018-04-09 15:13:21 +0200

    dimensionreduce.py added

--- kmeans.py
+++ kmeans.py
@@ -1,16 +1,11 @@
 from math import sqrt
-#from PIL import Image,ImageDraw
 import numpy as np
 from similarityMeasures import pearson
 import random
-
-def rotatematrix(data):
-    newdata = []
-    for i in range(len(data[0])):
-        newrow = [data[j][i] for j in range(len(data))]
-        newdata.append(newrow)
-    return newdata
-
+import pandas as pd
+import bokeh.plotting as bp
+from bokeh.models import HoverTool, BoxSelectTool
+from bokeh.plotting import figure, show, output_notebook
 
 def kcluster(rows, distance=pearson, k=4):
     # Determine the minimum and maximum values for each point
@@ -20,7 +15,7 @@ def kcluster(rows, distance=pearson, k=4):
 
     lastmatches = None
     for t in range(100):
-        print 'Iteration %d' % t
+        #print('Iteration %d' % t)
         bestmatches = [[] for i in range(k)]
         # Find which centroid is the closest for each row
         for j in range(len(rows)):
@@ -43,92 +38,50 @@ def kcluster(rows, distance=pearson, k=4):
                 for j in range(len(avgs)):
                     avgs[j] /= len(bestmatches[i])
                 clusters[i] = avgs
-	#Calculating Calinski and Harabaz Index
-	#c, center of E
-	c = [np.mean(L) for L in zip(*rows)]
-	W_k = sum([sum([distance(clusters[i], rows[j])**2 for j in bestmatches[i]]) for i in range(k)])
-	B_k = sum([distance(clusters[i], c)*len(bestmatches[i]) for i in range(k)])
-	s = ((B_k)/(W_k))*((len(rows)-k)/(k-1))
+    #Calculating Calinski and Harabaz Index
+    #c, center of E
+    c = [np.mean(L) for L in zip(*rows)]
+    W_k = sum([sum([distance(clusters[i], rows[j])**2 for j in bestmatches[i]]) for i in range(k)])
+    B_k = sum([distance(clusters[i], c)*len(bestmatches[i]) for i in range(k)])
+    s = ((B_k)/(W_k))*((len(rows)-k)/(k-1))
     return bestmatches, s
 
-
-
-def scaledown(data, distance=pearson, rate=0.01):
-    n = len(data)
-    # The real distances between every pair of items
-    realdist = [[distance(data[i], data[j]) for j in range(n)] for i in range(n)]
-    outersum = 0.0
-    # Randomly initialize the starting points of the locations in 2D
-    loc = [[random.random(), random.random()] for i in range(n)]
-    fakedist = [[0.0 for j in range(n)] for i in range(n)]
-
-    lasterror = None
-    for m in range(0, 1000):
-        # Find projected distances
-        for i in range(n):
-            for j in range(n):
-                fakedist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2) for x in range(len(loc[i]))]))
-
-        # Move points
-        grad = [[0.0, 0.0] for i in range(n)]
-
-        totalerror = 0
-        for k in range(n):
-            for j in range(n):
-                if j == k: continue
-                # The error is percent difference between the distances
-                errorterm = (fakedist[j][k] - realdist[j][k]) / realdist[j][k]
-
-                # Each point needs to be moved away from or towards the other
-                # point in proportion to how much error it has
-                grad[k][0] += ((loc[k][0] - loc[j][0]) / fakedist[j][k]) * errorterm
-                grad[k][1] += ((loc[k][1] - loc[j][1]) / fakedist[j][k]) * errorterm
-
-                # Keep track of the total error
-                totalerror += abs(errorterm)
-        #print(totalerror)
-
-        # If the answer got worse by moving the points, we are done
-        if lasterror and lasterror < totalerror: break
-        lasterror = totalerror
-
-        # Move each of the points by the learning rate times the gradient
-        for k in range(n):
-            loc[k][0] -= rate * grad[k][0]
-            loc[k][1] -= rate * grad[k][1]
-
-    return loc
-
 def optimized_kmeans(rows, distance=pearson):
-	list_data = []
-	for k in range(2,50):
-		print "------> k = " + str(k)
-		bestmatches, s = kcluster(rows, distance=pearson, k=k)
-		for i in range(10):
-			bestmatches_n, s_n = kcluster(rows, distance=pearson, k=k)
-			if s_n > s:
-				s = s_n
-				bestmatches = bestmatches_n
-		list_data.append([bestmatches, s])
-		if k > 4:
-			if list_data[-1][1] < list_data[-2][1]:
-				break
-	return k, max(list_data, key=lambda x: x[1])
-		
-				
-			
-	
-def draw2d(data, labels, jpeg='mds2d.jpg'):
-    img = Image.new('RGB', (2000, 2000), (255, 255, 255))
-    draw = ImageDraw.Draw(img)
-    for i in range(len(data)):
-        x = (data[i][0] + 0.5) * 1000
-        y = (data[i][1] + 0.5) * 1000
-        draw.text((x, y), labels[i].encode('latin-1','ignore'), (0, 0, 0))
-    img.save(jpeg, 'JPEG')
+    list_data = []
+    for k in range(2,50):
+        print("------> k = " + str(k))
+        bestmatches, s = kcluster(rows, distance=pearson, k=k)
+        for i in range(5):
+            bestmatches_n, s_n = kcluster(rows, distance=pearson, k=k)
+            print str(i)+': '+str(s_n)
+            if s_n > s:
+                s = s_n
+                bestmatches = bestmatches_n
+        list_data.append([bestmatches, s])
+        if k > 4:
+            if list_data[-1][1] < list_data[-2][1]:
+                break
+    return k, max(list_data, key=lambda x: x[1])
+
+def visualize(loc,bestmatches,rownames):
+    output_file("test.html")
+    colormap = np.array(["#6d8dca", "#69de53", "#723bca", "#c3e14c", "#c84dc9", "#68af4e", "#6e6cd5","#e3be38", "#4e2d7c", "#5fdfa8", "#d34690", "#3f6d31", "#d44427", "#7fcdd8", "#cb4053", "#5e9981","#803a62", "#9b9e39", "#c88cca", "#e1c37b", "#34223b", "#bdd8a3", "#6e3326", "#cfbdce", "#d07d3c","#52697d", "#7d6d33", "#d27c88", "#36422b", "#b68f79"])
+    plot_kmeans = bp.figure(plot_width=700, plot_height=600, title="KMeans clustering of the news",tools="pan,wheel_zoom,box_zoom,reset,hover,previewsave",x_axis_type=None, y_axis_type=None, min_border=1)
+    kmeans_df = pd.DataFrame(loc, columns=['x', 'y'])
+    cluster_list = [0]*len(rownames)
+    for i in range(1,len(bestmatches)):
+        for j in bestmatches[i]:
+            cluster_list[j]=i
+    kmeans_df['cluster'] = pd.DataFrame(cluster_list)
+    kmeans_df['title'] = pd.DataFrame(rownames)
+    kmeans_df['color'] = colormap[cluster_list]
+    plot_kmeans.scatter(x='x', y='y',color='color', source=kmeans_df)
+    hover = plot_kmeans.select(dict(type=HoverTool))
+    hover.tooltips={"title": "@title","cluster":"@cluster"}
+    show(plot_kmeans)
 
 def printcluster(clusters,rownames):
     for c in kcluster:
         for article in [rownames[r] for r in c]:
-            print article 
-        print '-------------------------------'
+            print(article)
+        print('-------------------------------')
